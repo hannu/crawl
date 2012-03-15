f528570af20464dbd461a101492707c58e338729
Hannu Pelkonen <hannup@gmail.com>
2012-03-15 15:52:41 +0200

    Refactor sinatra backend

--- sinatra-backend.rb
+++ sinatra-backend.rb
@@ -10,37 +10,44 @@ REPOSITORY_DIR = File.join(File.expand_path(File.dirname(__FILE__)), "repository
 class CrawlerWeb < Sinatra::Base
   set :static, true
 
+  def get_diff(path = nil, date = nil)
+    repo = Grit::Repo.new(REPOSITORY_DIR)
+    Dir.chdir(REPOSITORY_DIR) do
+      diffs = repo.log(nil, path, {:n => 10, :diff_filter => 'M'}).map{ |commit|
+        commit.diffs.map { |diff|
+          {
+            :id => "#{diff.b_path}/#{commit.date.to_i}",
+            :path => diff.b_path,
+            :date => commit.date,
+            :a_blob => Page.parse_from_string(diff.a_blob.data).to_hash,
+            :b_blob => Page.parse_from_string(diff.b_blob.data).to_hash
+          }
+        }
+      }.flatten
+      diffs = diffs.select{ |diff| diff[:path] == path} if path
+      diffs = diffs.select{ |diff| diff[:date].to_i.to_s == date} if date
+      diffs = diffs.first if diffs.size == 1
+      diffs
+    end    
+  end
+
   get '/' do
     File.read(File.join(settings.public_folder, 'index.html'))
   end
 
   get '/diffs.json' do
     content_type :json
-    repo = Grit::Repo.new(REPOSITORY_DIR)
-    Dir.chdir(REPOSITORY_DIR) do
-      repo.log(nil,nil,{:n => 10, :diff_filter => 'M'}).map{ |c|
-        c.diffs.map { |diff|
-          {
-            :id => "#{diff.b_path}/#{c.date.to_i}",
-            :path => diff.b_path,
-            :date => c.date,
-            :a_blob => Page.parse_from_string(diff.a_blob.data).to_hash,
-            :b_blob => Page.parse_from_string(diff.b_blob.data).to_hash
-          }
-        }
-      }.flatten.to_json
-    end
+    get_diff.to_json
   end
 
-  get '/diffs/:path/:id.json' do
+  get '/diffs/:site/:item.json' do
     content_type :json
-    filename = File.join(params[:path],params[:id])
-    repo = Grit::Repo.new(REPOSITORY_DIR)
-    Dir.chdir(REPOSITORY_DIR) do
-      repo.log(filename).map{ |commit|
-        Page.parse_from_string((commit.tree/filename).data).to_hash.merge({:date => commit.date, :path => filename})
-      }.to_json
-    end
+    get_diff("#{params[:site]}/#{params[:item]}").to_json
+  end
+
+  get '/diffs/:site/:item/:date.json' do
+    content_type :json
+    get_diff("#{params[:site]}/#{params[:item]}", params[:date]).to_json
   end
 end
 
